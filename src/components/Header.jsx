86a30f429bc6c410ece71c75e72071d5819f09d9
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-10 08:05:59 +0530

    refactor: Update Header component styling and remove unused code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Header = () => {
 
 
   return (
     <>
-      <header className="bg-background border-b border-red-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between">
-        <Link to="/">
-          <div className='flex items-center gap-2 bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate'>
+      <header className="bg-background border-b border-green-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between">
+          <div className='flex items-center gap-2 text-green-600'>
             <span className="text-xl sm:text-2xl font-bold">Krishi Kaushal</span>
           </div>
-        </Link>
         <div className="flex flex-col sm:flex-row items-center gap-4">
         </div>
 
