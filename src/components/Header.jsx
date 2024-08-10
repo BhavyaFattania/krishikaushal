2161008064ac677e33171b395f9e0663ea25c40f
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-10 10:16:27 +0530

    chore: Update Header component with logo and link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
 
-  return (
-    <>
-      <header className="bg-background border-b border-green-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between">
-          <div className='flex items-center gap-2 text-green-600'>
-            <img className='w-24' src="/logo.png" alt="" />
-            <span className="text-xl sm:text-2xl font-bold">Krishi Kaushal</span>
-          </div>
-        <div className="flex flex-col sm:flex-row items-center gap-4">
-        </div>
-
-      </header>
-    </>
-  );
+    return (
+        <>
+            <header className="bg-background border-b border-green-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between">
+                <Link to={"/"}>
+                    <div className='flex items-center gap-2 text-green-600'>
+                        <img className='w-24' src="/logo.png" alt="" />
+                        <span className="text-xl sm:text-2xl font-bold">Krishi Kaushal</span>
+                    </div>
+                    </Link>
+            </header>
+        </>
+    );
 };
 
 export default Header;
