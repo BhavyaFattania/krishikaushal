3af957ae607a1736a1206dd9dc5b79d74093faf9
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-10 15:59:43 +0530

    refactor: Update Footer component with responsive design and improved styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <footer className="bg-background border-t border-green-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-center">
-            <div className='flex justify-center items-center gap-2 text-green-600'>
+        <footer className="border-t border-green-600 rounded-full px-4 py-4 md:px-6 mt-[7.8rem] flex items-center justify-center">
+            <div className='flex justify-center items-center gap-2 text-green-50'>
             © 2024 <Link to={"/"}><span className="font-semi">Krishi Kaushal.</span> presenting to DA-IICT Hackout'24</Link>
             </div>
         </footer>
