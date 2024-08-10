d58e3b22aaabf81cc675984c3e0373817ed627c2
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-10 12:08:32 +0530

    feat: Add FarmerGuide page component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-
 import Header from './components/Header';
 import PlantAnalyze from './pages/PlantAnalyze';
 import './i18n';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer';
-
+import FarmerGuide from './pages/FarmerGuide';
 
 function App() {
   return (
     <BrowserRouter>
-      <div >
+      <div>
         <Header />
         <Routes>
           <Route index element={<Home />} />
           <Route path="/PlantAnalyze" element={<PlantAnalyze />} />
-
+          <Route path="/FarmerGuide" element={<FarmerGuide />} />
         </Routes>
         <Footer />
       </div>
