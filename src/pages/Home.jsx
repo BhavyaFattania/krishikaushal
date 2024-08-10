44047af96cbef1c730164975593f32c286e6e2d1
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-10 09:41:05 +0530

    feat: Add Home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FaMapSigns } from "react-icons/fa";
+import { MdSecurity } from "react-icons/md";
+import { FaSunPlantWilt } from "react-icons/fa6";
+
+import { Link } from 'react-router-dom'
+
+
+const Home = () => {
+
+    return (
+        <>
+            <div className='flex flex-col gap-6'>
+                <div className="container mx-auto mt-10 ">
+                    <h2 className="text-center text-5xl font-bold mb-4 text-green-800">Beej se Fasal tak</h2>
+                </div>
+                <div className="text-2xl font-semibold grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8 m-6">
+                    <Link to={"/"}>
+                        <div className="max-w-sm bg-white shadow-md rounded-lg p-6 border-2 border-green-600">
+                            <FaMapSigns />
+                            <p>Farmer Guide</p>
+                        </div>
+                    </Link>
+                    <Link to={"/"}>
+                        <div className="max-w-sm bg-white shadow-md rounded-lg p-6 border-2 border-green-600">
+                            <FaSunPlantWilt />
+                            <p>Plant Planner</p>
+                        </div>
+                    </Link>
+                    <Link to={"/PlantAnalyze"}>
+                        <div className="max-w-sm bg-white shadow-md rounded-lg p-6 border-2 border-green-600">
+                            <MdSecurity />
+                            <p>Plant Protector</p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Home
