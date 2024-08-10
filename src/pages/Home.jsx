f3c5262585b3bc806cfbfa001a7b15ebece5afc6
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-10 10:00:38 +0530

    feat: Add introductory text to Home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,9 @@ const Home = () => {
     return (
         <>
             <div className='flex flex-col gap-6'>
-                <div className="container mx-auto mt-10 ">
+                <div className="flex flex-col justify-center mx-auto mt-10 ">
                     <h2 className="text-center text-5xl font-bold mb-4 text-green-800">Beej se Fasal tak</h2>
+                    <p className='text-center max-w-4xl'>Krishi Kaushal aims to bridge the gap between traditional farming practices and modern technology by providing farmers with essential knowledge, predictive tools, and disease management solutions to optimize crop yield and ensure sustainable farming practices.</p>
                 </div>
                 <div className="text-2xl font-semibold grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8 m-6">
                     <Link to={"/"}>
