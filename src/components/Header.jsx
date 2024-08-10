11d0fedd66ff5a0df377c3c4313b5acf2150900b
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-10 15:59:52 +0530

    refactor: Update Header component with responsive logo size and alignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <>
-            <header className="bg-background border-b border-green-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between">
+            <header className="bg-green-600 border-b border-green-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between">
                 <Link to={"/"}>
                     <div className='flex items-center justify-between sm:justify-normal gap-2 text-green-600'>
                         <img className='w-16 sm:w-24 ' src="/logo.png" alt="" />
-                        <span className="text-xl sm:text-2xl font-bold">Krishi Kaushal</span>
+                        <span className="text-xl sm:text-2xl font-bold text-white">Krishi Kaushal</span>
                     </div>
                 </Link>
             </header>
